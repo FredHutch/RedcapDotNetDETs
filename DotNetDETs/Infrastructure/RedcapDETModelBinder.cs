40ca5a7d4e43fc9c37af0913ca88f44de26a65f6
Paul Litwin <paul@litwinconsulting.com>
2017-06-19 16:47:59 -0700

    Update model binder to add support for redcap_repeat fields

--- DotNetDETs/Infrastructure/RedcapDETModelBinder.cs
+++ DotNetDETs/Infrastructure/RedcapDETModelBinder.cs
@@ -13,6 +13,8 @@ namespace DotNetDETs.Infrastructure
         public string project_id { get; set; }
         public string record { get; set; }
         public string redcap_event_name { get; set; }
+        public string redcap_repeat_instrument { get; set; }
+        public string redcap_repeat_instance { get; set; }
         public string redcap_data_access_group { get; set; }
         public string instrument { get; set; }
         public string complete_flag { get; set; }
@@ -48,10 +50,25 @@ namespace DotNetDETs.Infrastructure
             //    Log.DebugFormat("REDCap DET Post {0}:{1}:{2}", i, controls.GetKey(i), controls.Get(i));
             //}
 
-            try {
+            try
+            {
                 // Grab the values of the fields and populate the RedcapDET object.
-                redcapDet.project_id = controls["project_id"].ToString();
-                redcapDet.record = controls["record"].ToString();
+                try
+                {
+                    redcapDet.project_id = controls["project_id"].ToString();
+                }
+                catch (Exception ex)
+                {
+                    Log.ErrorFormat("project_id model binder exception:{0}. Inner Exception:{1}", ex.Message, ex.InnerException.Message);
+                }
+                try
+                {
+                    redcapDet.record = controls["record"].ToString();
+                }
+                catch (Exception ex)
+                {
+                    Log.ErrorFormat("record model binder exception:{0}. Inner Exception:{1}", ex.Message, ex.InnerException.Message);
+                }
                 try
                 {
                     redcapDet.redcap_event_name = controls["redcap_event_name"].ToString();
@@ -62,18 +79,48 @@ namespace DotNetDETs.Infrastructure
                 }
                 try
                 {
+                    redcapDet.redcap_repeat_instrument = controls["redcap_repeat_instrument"].ToString();
+                }
+                catch
+                {
+                    redcapDet.redcap_repeat_instrument = "";
+                }
+                try
+                {
+                    redcapDet.redcap_repeat_instance = controls["redcap_repeat_instance"].ToString();
+                }
+                catch
+                {
+                    redcapDet.redcap_repeat_instance = "";
+                }
+                try
+                {
                     redcapDet.redcap_data_access_group = controls["redcap_data_access_group"].ToString();
                 }
                 catch
                 {
                     redcapDet.redcap_data_access_group = "";
                 }
-                redcapDet.instrument = controls["instrument"].ToString();
-                redcapDet.complete_flag = controls[redcapDet.instrument + "_complete"].ToString();
+                try
+                {
+                    redcapDet.instrument = controls["instrument"].ToString();
+                }
+                catch (Exception ex)
+                {
+                    Log.ErrorFormat("instrument model binder exception:{0}. Inner Exception:{1}", ex.Message, ex.InnerException.Message);
+                }
+                try
+                {
+                    redcapDet.complete_flag = controls[redcapDet.instrument + "_complete"].ToString();
+                }
+                catch (Exception ex)
+                {
+                    Log.ErrorFormat("instrument complete model binder exception:{0}. Inner Exception:{1}", ex.Message, ex.InnerException.Message);
+                }
             }
             catch (Exception ex)
             {
-                Log.ErrorFormat("Model binder exception:{0};{1}", ex.Message, ex);
+                Log.ErrorFormat("Model binder exception:{0}. Inner Exception:{1}", ex.Message, ex.InnerException.Message);
             }
 
             bindingContext.Model = redcapDet;
